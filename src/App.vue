35dcefd081b098bd4972e8e4dbf79715743016bb
柒木楠 <2485669746@qq.com>
2024-05-04 16:49:01 +0800

    插槽未完待续....

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,14 @@
         <PersionDemo></PersionDemo>
         <h1>*****</h1>
         <fatherS></fatherS>
+        <p>******</p>
+        <slotS></slotS>
     </div>
 </template>
 
 <script setup lang="ts">
 import fatherS from './views/fateherS.vue'
+import slotS from './views/slotS.vue'
 </script>
 
 <style scoped>
---
+++ src/views/slotS.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>插槽</p>
+        <MySlot><span style="color: red;">我是组件上的内容</span></MySlot>
+        <MySlot></MySlot>
+        <juming>
+            <template #header>
+                <div>我是具名插槽头部</div>
+            </template>
+            <template #footer>
+                <div>我是具名插槽底部</div>
+            </template>
+            <!-- <template #default> -->
+                <!-- 默认的隐式插槽 -->
+                <div>我是没有具名插槽的插槽</div>
+            <!-- </template> -->
+        </juming>
+        <juming v-slot="x">
+            {{ x.text }}--{{ x.count }}
+        </juming>
+    </div>
+</template>
+
+<script setup lang="ts">
+import MySlot from './slotOut.vue'
+import juming from './jumingSlot.vue'
+</script>
+
+<style scoped></style>
