9a273f549efd6cf6bdc35d34d99155c7faa11d28
柒木楠 <2485669746@qq.com>
2024-05-04 23:51:57 +0800

    review solt and props

--- src/views/fateherS.vue
+++ src/views/fateherS.vue
@@ -5,12 +5,18 @@
         <h1>*********</h1>
         <p>{{ resonvalue }}</p>
         <Son :fmsg="fatherMsg" @message="reson"></Son>
+        <ftoS>
+            <template #a>
+            <div>这是父组件</div>
+        </template>
+        </ftoS>
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import Son from './sonS.vue'
+import ftoS from './ftoS.vue'
 const fatherMsg = ref<string>('我是父组件传递消息')
 const resonvalue = ref<string>('')
 const reson = (msg: string) => {
---
+++ src/views/srFat.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>{{msg}}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    msg: string
+}>()
+</script>
+
+<style scoped>
+
+</style>
