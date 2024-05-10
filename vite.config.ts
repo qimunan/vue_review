3226dfb470186bd6c77703154656bbb11aeaef18
柒木楠 <2485669746@qq.com>
2024-05-10 23:38:42 +0800

    feat(新增打包后视图，方便后续对其优化): :sparkles: 新增打包后视图，方便对其进行优化

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { visualizer } from "rollup-plugin-visualizer";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,7 +19,14 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()]
-    })
+    }),
+    visualizer({
+      gzipSize: true,
+      brotliSize: true,
+      emitFile: false,
+      filename: "view.html", //分析图生成的文件名
+      open: true //如果存在本地服务端口，将在打包后自动展示
+    }),
   ],
   resolve: {
     alias: {
