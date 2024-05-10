7a8d831fc19d86f82fc55a754d10e30dd4670e3f
柒木楠 <2485669746@qq.com>
2024-05-10 14:29:13 +0800

    feat: :sparkles: 后台首页搭建待完成

--- src/views/doubleBlind.vue
+++ src/views/doubleBlind.vue
@@ -7,6 +7,31 @@ function update() {
   model.value++
 }
 console.log(model)
+import { onBeforeMount,onMounted, onBeforeUpdate,onUpdated, onBeforeUnmount, onUnmounted } from 'vue'
+
+
+onBeforeMount(() => {
+  console.log('onBeforeMount: 组件将要挂载');
+});
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate: 组件将要更新');
+});
+onBeforeUnmount(() => {
+  console.log('onBeforeUnmount: 组件将要卸载');
+});
+onMounted(() => {
+  console.log('onMounted: 组件已挂载');
+});
+
+// 在组件更新后执行的操作
+onUpdated(() => {
+  console.log('onUpdated: 组件已更新');
+});
+
+// 在组件卸载前执行的操作
+onUnmounted(() => {
+  console.log('onUnmounted: 组件将被卸载');
+});
 </script>
 
 <template>
